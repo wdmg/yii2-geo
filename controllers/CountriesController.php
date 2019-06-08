cf11e700c00f50c8dafd2a39fab20197b8bf6dad
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-08 14:34:04 +0300

    Ignore RBAC roles if AuthManager not configured

--- controllers/CountriesController.php
+++ controllers/CountriesController.php
@@ -21,7 +21,7 @@ class CountriesController extends Controller
      */
     public function behaviors()
     {
-        return [
+        $behaviors = [
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -32,12 +32,27 @@ class CountriesController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'allow' => true,
-                        'roles' => ['admin']
+                        'roles' => ['admin'],
+                        'allow' => true
                     ],
                 ],
             ],
         ];
+
+        // If auth manager not configured use default access control
+        if(!Yii::$app->authManager) {
+            $behaviors['access'] = [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'roles' => ['@'],
+                        'allow' => true
+                    ],
+                ]
+            ];
+        }
+
+        return $behaviors;
     }
 
     /**
--- controllers/GeoController.php
+++ controllers/GeoController.php
@@ -4,6 +4,7 @@ namespace wdmg\geo\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 
 /**
@@ -16,17 +17,38 @@ class GeoController extends Controller
      */
     public function behaviors()
     {
-        return [
+        $behaviors = [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
             'access' => [
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'allow' => true,
-                        'roles' => ['admin']
+                        'roles' => ['admin'],
+                        'allow' => true
                     ],
                 ],
             ],
         ];
+
+        // If auth manager not configured use default access control
+        if(!Yii::$app->authManager) {
+            $behaviors['access'] = [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'roles' => ['@'],
+                        'allow' => true
+                    ],
+                ]
+            ];
+        }
+
+        return $behaviors;
     }
 
     /**
